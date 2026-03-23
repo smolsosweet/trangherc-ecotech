d32783ebe7c3c32689ff418f4786efe882e94a86
smolsosweet <trungtinhpro.28@gmail.com>
2026-03-23 13:44:22 +0700

    remove BOM

--- src/main/java/com/trangherb/ecotech/HomeController.java
+++ src/main/java/com/trangherb/ecotech/HomeController.java
@@ -1,4 +1,4 @@
-﻿package com.trangherb.ecotech;
+package com.trangherb.ecotech;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/trangherb/ecotech/TrangherbEcotechApplication.java
+++ src/main/java/com/trangherb/ecotech/TrangherbEcotechApplication.java
@@ -1,4 +1,4 @@
-﻿package com.trangherb.ecotech;
+package com.trangherb.ecotech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
